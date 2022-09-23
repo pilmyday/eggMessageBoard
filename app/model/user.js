43d0f8618f9f583acf3a7a8afcc52a1ed43b8582
pli0428 <114226744+pli0428@users.noreply.github.com>
2022-09-23 18:11:49 +0800

    Update user.js

--- app/model/user.js
+++ app/model/user.js
@@ -2,15 +2,15 @@ module.exports = app => {
     const { STRING, INTEGER, DATE } = app.Sequelize;
 
     const User = app.model.define('user', {
-        user_id: {
+        userId: {
             type: INTEGER,
             primaryKey: true,
             autoIncrement: true,
         },
-        user_name: STRING(30),
+        userName: STRING(30),
         comment: STRING(60),
-        created_at: DATE,
-        updated_at: DATE,
+        createdAt: DATE,
+        updatedAt: DATE,
     });
 
     return User;
